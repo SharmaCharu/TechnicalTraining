4fc69c86ff382db251ab59deeb570449db8c5200
SharmaCharu <53037307+SharmaCharu@users.noreply.github.com>
2019-07-18 14:24:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var person={
+    name:'Charu',
+    age:20,
+    isMarried:false,
+};
+var age1=3;
+var category1=getCategory(age1);
+console.log(Category);
+
+function Category(age)
+{
+    if(age >=0 && age<5)
+    Category=baby;
+}
